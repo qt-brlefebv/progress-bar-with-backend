2e9607f77da059849c201e0c8893398062ff22ec
Brendan LeFebvre <brendan.lefebvre@qt.io>
2024-03-04 14:05:54 -0500

    Added ProgressBackend class via Qt Creator

---
+++ backend/progressbackend.cpp
@@ -0,0 +1,3 @@
+#include "progressbackend.h"
+
+ProgressBackend::ProgressBackend() {}
---
+++ backend/progressbackend.h
@@ -0,0 +1,15 @@
+#ifndef PROGRESSBACKEND_H
+#define PROGRESSBACKEND_H
+
+#include <QObject>
+#include <QQmlEngine>
+
+class ProgressBackend
+{
+    Q_OBJECT
+    QML_ELEMENT
+public:
+    ProgressBackend();
+};
+
+#endif // PROGRESSBACKEND_H
